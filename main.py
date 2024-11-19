56b8e85f3497d3a9adc92f47d007446c76292880
Anpilov Andrey <62002015+AnpilovAA@users.noreply.github.com>
2024-11-19 07:57:14 +0300

    it will hide the functions define_my_coordinates and distance_calculation

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def get_all_info_coffee(all_info_coffee: dict) -> list:
     for info in all_info_coffee:
         main_info = {
             "title": info['Name'],
-            "latitude": info['Latitude_WGS84'],
             "longitude": info['Longitude_WGS84'],
+            "latitude": info['Latitude_WGS84'],
         }
         accumulate.append(main_info)
     return accumulate
@@ -25,19 +25,14 @@ def define_my_coordinates() -> tuple:
     return longitude_luntitude
 
 
-def distance_calculation(coffee_location: list, my_coordinates: tuple) -> tuple:
-    return distance(my_coordinates, coffee_location).km
-
-
 def combine_data(coffee_houses: list) -> tuple:
-    my_coordinates = define_my_coordinates()
     coffee_house_with_distance = []
     for coffee_house in coffee_houses:
         coffee_location = (coffee_house["longitude"], coffee_house["latitude"])
-        distance = distance_calculation(coffee_location=coffee_location, my_coordinates=my_coordinates)
-        coffee_house.update(distance=distance)
+        distance_km = distance(my_coordinates, coffee_location).km
+        coffee_house.update(distance=distance_km)
         coffee_house_with_distance.append(coffee_house)
-    return coffee_house_with_distance, my_coordinates
+    return coffee_house_with_distance
 
 
 def coffee_near_me(coffee_houses_with_distance):
@@ -75,7 +70,8 @@ if __name__ == "__main__":
         coffee_json = read_json.read()
         coffee = loads(coffee_json)
     coffee_houses = get_all_info_coffee(coffee)
-    coffee_with_distance, my_coordinates = combine_data(coffee_houses=coffee_houses)
+    my_coordinates = fetch_coordinates(KEY, input("Гдк вы находитесь?: "))
+    coffee_with_distance = combine_data(coffee_houses=coffee_houses)
     nearest_5 = sorted(coffee_with_distance, key=coffee_near_me)[:5]
     lantitude, longitude = my_coordinates
     map_html((longitude, lantitude), nearest_5)
